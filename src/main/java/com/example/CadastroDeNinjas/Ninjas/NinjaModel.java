431317866dc900470ae11b1f537c4a47aec62be4
Marcos Fernandes <marcos.fernandes0324@gmail.com>
2025-01-13 22:19:20 -0300

    Atualiza para usar dependencia Lombok

--- src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/com/example/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -2,6 +2,7 @@ package com.example.CadastroDeNinjas.Ninjas;
 
 import com.example.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
@@ -9,6 +10,9 @@ import java.util.List;
 /* JPA = Java Persistence API*/
 @Entity
 @Table(name = "tb_cadastro")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class NinjaModel {
 
     @Id
@@ -22,39 +26,4 @@ public class NinjaModel {
     @ManyToOne
     @JoinColumn(name = "missoes_id") /*Foreing Key ou Chave Estrangeira*/
     private MissoesModel missoes;
-
-    /*No-args constructor*/
-    public NinjaModel() {
-    }
-
-    /*All-args constructor*/
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
